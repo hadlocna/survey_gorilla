851fc91e33deeff38d7f14f48d930b7bbe53c5ae
Apprentice <apprentice@dbc-chicago-021.local>
2013-11-10 08:08:03 -0600

    new questions default to 2 choices, add choice button glitch is fixed so that it doesnt always add to most recent question

--- public/js/application.js
+++ public/js/application.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
   var surveyForm = $('.survey-form');
   var i = 0;
 
-  $('.add-question').on('click', function(event) {
+  $('form').on('click', '.add-question', function(event) {
     event.preventDefault();
     i++;
-    surveyForm.prepend("<p class='question" + i +"'><label>" + i + " <input type='text' name='question" + i + "'placeholder='new question text'></label></p><p><button class='add-choice'>add choice</button></p>");
-    $(".question" + i).append("<p><label> <input type='text' name='q" + i + "choice1' placeholder='new choice text'></label></p>")
+    surveyForm.prepend("<p class='question" + i +"' data-num='" + i + "'><label>" + i + " <input type='text' name='question" + i + "'placeholder='new question text'></label></p><p><button class='add-choice'>add choice</button></p>");
+    var count = 0
+    while(count < 2) {
+    $(".question" + i).append("<p><label> <input type='text' name='q" + i + "choice1' placeholder='new choice text'></label></p>");
+    count ++;
+    }
   });
   $('.survey-form').on('click', '.add-choice', j = 1 , function(event) {
     event.preventDefault();
     j++;
-    $(".question" + i).append("<p><label> <input type='text' name='q" + i + "choice" + j + "' placeholder='new choice text'></label></p>");
+    var question = $(this).parent().prev()
+    var num = question.data('num')
+    question.append("<p><label> <input type='text' name='q" + num + "choice" + j + "' placeholder='new choice text'></label></p>");
   });
 
 });
