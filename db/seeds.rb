41f3f94fe14474fa6234c14bb79abe9619d18df5
Apprentice <apprentice@dbc-chicago-021.local>
2013-11-09 20:26:45 -0600

    added some seed data with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 user = User.new(name: 'fred', email: 'fred@dbc.com')
 user.password = '123'
 user.save!
@@ -9,15 +11,13 @@ survey = user.authored_surveys.create(title: 'how many salamanders in a maelstro
 survey.survey_takers.create(user_id: 2)
 q = survey.questions.create(text: 'how many?')
 
-choice = q.choices.create(text: '1')
+# choice = q.choices.create(text: '1')
 q.choices.create(text: '2')
 q.choices.create(text: '3')
 q.choices.create(text: '5')
 q.choices.create(text: '47')
 
-
-choice.responses.create(survey_taker_id: 1)
-
+# choice.responses.create(survey_taker_id: 1)
 
 q2 = survey.questions.create(text: 'how much?')
 q2.choices.create(text: '22')
@@ -25,3 +25,24 @@ q2.choices.create(text: '2')
 q2.choices.create(text: '6')
 q2.choices.create(text: '9')
 q2.choices.create(text: '27')
+
+
+
+10.times do |i|
+  u = User.new(name: Faker::Name.first_name, email: Faker::Internet.email)
+  u.password = '123'
+  u.save!
+end
+
+10.times do |i|
+  s = Survey.new(title: Faker::Lorem.word)
+  User.all.sample.authored_surveys << s
+  5.times do |j|
+    q = Question.new(text: Faker::Lorem.word)
+    4.times do |k|
+      q.choices.new(text: Faker::Lorem.word)
+    end
+    s.questions << q
+  end
+  s.save!
+end
