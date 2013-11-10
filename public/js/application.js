ea2570d7e65b3647848aec5632cfd4de05144c73
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 15:30:03 -0600

    prettified survey creation, bug fixes

--- public/js/application.js
+++ public/js/application.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
   $('form').on('click', '.add-question', function(event) {
     event.preventDefault();
     i++;
-    surveyForm.prepend("<p class='question" + i +"' data-num='" + i + "'><label>" + i + " <input type='text' name='question" + i + "'placeholder='new question text'></label></p><p><button class='add-choice'>add choice</button></p>");
+    surveyForm.prepend("<br><section class='question" + i +"' data-num='" + i + "'><input type='text' name='question" + i + "'placeholder='Question'><button class='add-choice cool-button button-right'>Add Choice</button></section>");
     var count = 0
     while(count < 2) {
-    $(".question" + i).append("<p><label> <input type='text' name='q" + i + "choice" + count + "' placeholder='new choice text'></label></p>");
+    $(".question" + i).append("<section><input type='text' name='q" + i + "choice" + count + "' placeholder='Choice'></section>");
     count ++;
     }
   });
   $('.survey-form').on('click', '.add-choice', j = 1 , function(event) {
     event.preventDefault();
     j++;
-    var question = $(this).parent().prev()
+    var question = $(this).parent()
     var num = question.data('num')
-    question.append("<p><label> <input type='text' name='q" + num + "choice" + j + "' placeholder='new choice text'></label></p>");
+    question.append("<section><input type='text' name='q" + num + "choice" + j + "' placeholder='Choice'></section>");
   });
 
 });
