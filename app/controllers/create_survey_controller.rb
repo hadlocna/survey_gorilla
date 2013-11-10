ae31819c6901861a130f398429b617cdaeebdcfa
Apprentice <apprentice@dbc-chicago-021.local>
2013-11-10 08:38:38 -0600

    still refactoring post request

--- app/controllers/create_survey_controller.rb
+++ app/controllers/create_survey_controller.rb
@@ -7,13 +7,6 @@ end
 post '/create_survey' do 
   # author will be User.find(session[:user_id])
   # user = User.find(session[:user_id])
-  p params
-  p'#################################'
-  p'#################################'
-  p'#################################'
-  p'#################################'
-  p'#################################'
-  p'#################################'
   survey = Survey.new(title: params[:title], author_id: session[:user_id])
   write_survey(survey)
  redirect '/'
