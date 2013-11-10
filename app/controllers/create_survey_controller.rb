a21d21d869f960be155aca5fb0fd0c285c9fee9c
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 13:37:23 -0600

    fixed survey creation bug, photo display conditional

--- app/controllers/create_survey_controller.rb
+++ app/controllers/create_survey_controller.rb
@@ -1,6 +1,6 @@
 require 'debugger'
 
-get '/create_survey' do 
+get '/create_survey' do
   if logged_in?
     # @user_id = session[:user_id]
     erb :create_survey
@@ -9,10 +9,10 @@ get '/create_survey' do
   end
 end
 
-post '/create_survey' do 
+post '/create_survey' do
   # author will be User.find(session[:user_id])
   # user = User.find(session[:user_id])
-  survey = Survey.new(title: params[:title], author_id: session[:user_id])
+  survey = Survey.new(title: params[:title], author_id: session[:id])
   write_survey(survey)
  redirect '/'
 end
